62190bd1176e24a37a581cd5c8fe51e894276902
Matt Home <matthome@Matts-MacBook-Pro.local>
2024-09-12 22:51:50 +0100

    Only 3 errors. lol

--- RPS.c
+++ RPS.c
@@ -10,10 +10,14 @@ int humanMove();
 int moveAssignInt (char answer);
 
 
+int answersComparison;
 char answer;
-int humanMoveChoice;
+char humanMoveChoice;
 int rndNo;
 int algoAnswer;
+const char compMove[10];
+
+
 
 //generate random number between 1 and 3
 int random_number(int min, int max) {
@@ -34,16 +38,16 @@ char compMoveConversion (int rndNoParam) {
     }
 }
 
-//accept human move
+//Prompt and accept human move
 int humanMove () {
 printf("Choose Rock, Paper or Scissors. (R, P, or S)\n");
 scanf("%c", &answer);
     if (answer == 'r') {
-        humanMoveChoice = 1;
+        humanMoveChoice = 'r';
     } else if (answer == 'p') {
-        humanMoveChoice = 2;
+        humanMoveChoice = 'p';
     } else if (answer == 's') {
-        humanMoveChoice = 3;
+        humanMoveChoice = 's';
     } else {
         humanMove();
     }
@@ -69,6 +73,17 @@ int moveAssignInt (char answer) {
 
 
 // compare user input with computer move
+// think not working as if statement not inside a function
+//therefore syntax wrong... will have to create function
+//rather than assigned fucntion
+
+int answersComparison = strcmp(humanMoveChoice, &compMove);
+if (answersComparison == 1) {
+    printf("That's a draw!");
+    humanMove();
+}
+
+
 // C does not allow string comparison
 //The reason for this is because != and == will only
 //compare the base addresses of those strings.
